b6a07bd114b304369b0da477fb7226d7f4523836
timmyreilly <timreilly@live.com>
2015-12-28 16:39:07 -0800

    initial commit

---
+++ GettingStartedWithKnockout/scripts/home.js
@@ -0,0 +1,9 @@
+﻿/// <reference path= "/scripts/knockout-3.4.0.debug.js" />
+
+$(function () {
+    var viewModel = {
+        firstName: ko.observable("John")
+    };
+
+    ko.applyBindings(viewModel);
+})
